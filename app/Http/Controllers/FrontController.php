0dd1278e692990db168b7c3f4674f6144bfae028
Guruhg19 <guruhvitra19@gmail.com>
2025-04-23 22:05:37 +0700

    Add main index page layout with navigation, categories, and featured sections

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index(){
+        $categories = Category::all();
+        return view('front.index', compact('categories'));
+    }
 }
